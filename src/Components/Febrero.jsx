dc192142d85088ab4da626be3e95321bb3e7dc1e
Dylan Fernandez <127891562+Dynezel@users.noreply.github.com>
2025-02-09 15:36:28 -0300

    Add files via upload

---
+++ src/Components/Febrero.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+import "../css/tablas.css";
+
+export default function Febrero() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Fecha</th>
+          <th>Detalles</th>
+          <th>Ingresos</th>
+          <th>Egresos</th>
+          <th>Total</th>
+        </tr>
+        <tr>
+          <td>01/02</td>
+          <td className="restante">Restante Enero</td>
+          <td>575.000</td>
+          <td></td>
+          <td>575.000</td>
+        </tr>
+        <tr>
+          <td>03/02</td>
+          <td className="ingresos">Luis Carabajal, Manzana 3 Lote 12</td>
+          <td>25.000</td>
+          <td></td>
+          <td>600.000</td>
+        </tr>
+        <tr>
+          <td>03/02</td>
+          <td className="ingresos">Claudia Mazzini, Manzana 28, Lotes 7 y 9</td>
+          <td>25.000</td>
+          <td></td>
+          <td>625.000</td>
+        </tr>
+        <tr>
+          <td>05/02</td>
+          <td className="ingresos">Ovidio Vera, Manzana 21, Lote 2</td>
+          <td>25.000</td>
+          <td></td>
+          <td>650.000</td>
+        </tr>
+        <tr>
+          <td>06/02</td>
+          <td className="ingresos">Johanna Mayhua, Manzana 11, Lotes 7 y 9</td>
+          <td>25.000</td>
+          <td></td>
+          <td>675.000</td>
+        </tr>
+        <tr>
+          <td>06/02</td>
+          <td className="ingresos">Isabel Gonzalez, Manzana 15, Lote 7</td>
+          <td>25.000</td>
+          <td></td>
+          <td>700.000</td>
+        </tr>
+        <tr>
+          <td>06/02</td>
+          <td className="ingresos">
+            Pablo Silva, Manzana 7, Lotes 3 y 4 <br /> Manzana 5, Lote 2{" "}
+          </td>
+          <td>50.000</td>
+          <td></td>
+          <td>750.000</td>
+        </tr>
+        <tr>
+          <td>06/02</td>
+          <td className="ingresos">Graciela Almiron, Manzana 1, Lote 17</td>
+          <td>25.000</td>
+          <td></td>
+          <td>775.000</td>
+        </tr>
+        <tr>
+          <td>07/02</td>
+          <td className="ingresos">Johana Gonzalez, Manzana 17, Lote 8</td>
+          <td>25.000</td>
+          <td></td>
+          <td>800.000</td>
+        </tr>
+        <tr>
+          <td>07/02</td>
+          <td className="ingresos">Dorila Tesen Chavez, Manzana 23 Lote 2</td>
+          <td>30.000</td>
+          <td></td>
+          <td>830.000</td>
+        </tr>
+        <tr>
+          <td>07/02</td>
+          <td className="ingresos">Diego Pergar, Manzana 7 Lote 5</td>
+          <td>20.000</td>
+          <td></td>
+          <td>850.000</td>
+        </tr>
+        <tr>
+          <td>07/02</td>
+          <td className="ingresos">Adrian Valenzuela, Manzana 17 Lote 4</td>
+          <td>25.000</td>
+          <td></td>
+          <td>875.000</td>
+        </tr>
+        <tr>
+          <td>07/02</td>
+          <td className="ingresos">Yamila Gartner, Manzana 2 Lote 16</td>
+          <td>25.000</td>
+          <td></td>
+          <td>900.000</td>
+        </tr>
+        <tr>
+          <td>07/02</td>
+          <td className="ingresos">Maidana Saravia, Manzana 28 Lote 20</td>
+          <td>20.000</td>
+          <td></td>
+          <td>920.000</td>
+        </tr>
+        <tr>
+          <td>07/02</td>
+          <td className="ingresos">Cristian David Anaya</td>
+          <td>25.000</td>
+          <td></td>
+          <td>945.000</td>
+        </tr>
+        <tr>
+          <td>09/02</td>
+          <td className="ingresos">Cristian Iraia, Manzana 6 Lote 16</td>
+          <td>25.000</td>
+          <td></td>
+          <td>970.000</td>
+        </tr>
+        <tr>
+          <td>09/02</td>
+          <td className="ingresos">Fanny Cabrera, Manzana 3 Lote 15</td>
+          <td>25.000</td>
+          <td></td>
+          <td>995.000</td>
+        </tr>
+        <tr>
+          <td>09/02</td>
+          <td className="ingresos">Ursulina Benegas, Manzana 5 Lote 13</td>
+          <td>25.000</td>
+          <td></td>
+          <td>1.020.000</td>
+        </tr>
+        <tr>
+          <td>09/02</td>
+          <td className="ingresos">Delgado Maria, Manzana 22 Lote 11</td>
+          <td>50.000</td>
+          <td></td>
+          <td>1.070.000</td>
+        </tr>
+        <tr>
+          <td>09/02</td>
+          <td className="ingresos">Mariela Noemi, Manzana 26 Lote 5 y 6</td>
+          <td>25.000</td>
+          <td></td>
+          <td>1.095.000</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
